fbfbd9d069ba301559d04272f75f5a9ed3c0b50d
CrebboElodie <100001938+CrebboElodie@users.noreply.github.com>
2023-04-13 11:38:14 +0100

    Add files via upload

---
+++ dnd5e-generator.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+var level int
+var class string
+var classes = [12]string{"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"}
+var background string
+var backgrounds = [6]string{"Acolyte", "Criminal", "Folk Hero", "Noble", "Sage", "Soldier"}
+var race string
+var races = [9]string{"Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Teifling"}
+var alignment string
+var alignments = [3][3]string{{"Lawful Good", "Neutral Good", "Chaotic Good"}, {"Lawful Neutral", "True Neutral", "Chaotic Neutral"}, {"Lawful Evil", "Neutral Evil", "Chaotic Evil"}}
+var health int
+var stats = [6]int{}
+var stats_name = [6]string{"Str", "Dex", "Con", "Int", "Wis", "Cha"}
+
+func main() {
+
+	file, err := os.Open("./values.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	reader := csv.NewReader(file)
+	records, _ := reader.ReadAll()
+
+	fmt.Println(records)
+
+	level = rand.Intn(20 - 1)
+	fmt.Printf("Lv:%d\n", level)
+
+	class = classes[rand.Intn(11-0)]
+	fmt.Printf("Class:%s\n", class)
+
+	background = backgrounds[rand.Intn(5-0)]
+	fmt.Printf("Background:%s\n", background)
+
+	race = races[rand.Intn(8-0)]
+	fmt.Printf("Race:%s\n", race)
+
+	alignment = alignments[rand.Intn(2-0)][rand.Intn(2-0)]
+	fmt.Printf("Alignment:%s\n", alignment)
+
+	health = rand.Intn(20 - 1)
+	fmt.Printf("Health:%d\n", health)
+
+	for index := 0; index <= 5; index++ {
+		stats[index] = rand.Intn(20 - 1)
+		fmt.Printf("%s:%d\n", stats_name[index], stats[index])
+	}
+}
